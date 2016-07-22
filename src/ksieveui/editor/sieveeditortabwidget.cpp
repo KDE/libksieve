1d477a033c3b991dd50c2c2c605066788d18ff36
Montel Laurent <montel@kde.org>
2016-07-22 13:41:52 +0200

    Move all in webengine directory

--- src/ksieveui/editor/sieveeditortabwidget.cpp
+++ src/ksieveui/editor/sieveeditortabwidget.cpp
@@ -17,7 +17,7 @@
    Boston, MA 02110-1301, USA.
 */
 #include "sieveeditortabwidget.h"
-#include "ksieveui/editor/sieveeditorhelphtmlwidget.h"
+#include "webengine/sieveeditorhelphtmlwidget.h"
 
 #include <KLocalizedString>
 #include <KStringHandler>
