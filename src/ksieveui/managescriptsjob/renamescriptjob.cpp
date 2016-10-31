c856b69e695d98e1a8b6e240be544b727fefb260
Montel Laurent <montel@kde.org>
2016-10-31 20:45:25 +0100

    Add job to rename script

--- src/ksieveui/managescriptsjob/renamescriptjob.cpp
+++ src/ksieveui/managescriptsjob/renamescriptjob.cpp
@@ -1,6 +1,114 @@
+/*
+  Copyright (c) 2016 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
 #include "renamescriptjob.h"
 
-RenameScriptJob::RenameScriptJob(QObject *parent) : QObject(parent)
+#include <kmanagesieve/sievejob.h>
+
+using namespace KSieveUi;
+
+class KSieveUi::RenameScriptJobPrivate
+{
+public:
+    RenameScriptJobPrivate()
+        : mIsActive(false)
+    {
+    }
+    QString mNewName;
+    QUrl mOldUrl;
+    bool mIsActive;
+};
+
+RenameScriptJob::RenameScriptJob(QObject *parent)
+    : QObject(parent),
+      d(new RenameScriptJobPrivate)
 {
 
+}
+
+RenameScriptJob::~RenameScriptJob()
+{
+    delete d;
+}
+
+void RenameScriptJob::setOldUrl(const QUrl &url)
+{
+    d->mOldUrl = url;
+}
+
+void RenameScriptJob::setIsActive(bool active)
+{
+    d->mIsActive = active;
+}
+
+void RenameScriptJob::setNewName(const QString &newName)
+{
+    d->mNewName = newName;
+}
+
+bool RenameScriptJob::canStart() const
+{
+    return !d->mNewName.isEmpty() && !d->mOldUrl.isEmpty();
+}
+
+void RenameScriptJob::start()
+{
+    if (canStart()) {
+        KManageSieve::SieveJob *job = KManageSieve::SieveJob::get(d->mOldUrl);
+        connect(job, &KManageSieve::SieveJob::result, this, &RenameScriptJob::slotGetResult);
+    } else {
+        Q_EMIT finished(false);
+        deleteLater();
+    }
+}
+
+void RenameScriptJob::slotGetResult(KManageSieve::SieveJob *job, bool success, const QString &script, bool isActive)
+{
+    Q_UNUSED(job);
+    Q_UNUSED(isActive);
+    if (!success) {
+        Q_EMIT finished(false);
+        deleteLater();
+        return;
+    }
+    QUrl u = d->mOldUrl;
+    u = u.adjusted(QUrl::RemoveFilename);
+    u.setPath(u.path() +  QLatin1Char('/') + d->mNewName);
+
+    KManageSieve::SieveJob *putJob = KManageSieve::SieveJob::put(u, script, d->mIsActive, d->mIsActive);
+    connect(putJob, &KManageSieve::SieveJob::result, this, &RenameScriptJob::slotPutScript);
+}
+
+void RenameScriptJob::slotPutScript(KManageSieve::SieveJob *job, bool success)
+{
+    Q_UNUSED(job);
+    if (!success) {
+        Q_EMIT finished(false);
+        deleteLater();
+        return;
+    }
+    KManageSieve::SieveJob *deleteJob = KManageSieve::SieveJob::del(d->mOldUrl);
+    connect(deleteJob, &KManageSieve::SieveJob::result, this, &RenameScriptJob::slotDeleteResult);
+}
+
+void RenameScriptJob::slotDeleteResult(KManageSieve::SieveJob *job, bool success)
+{
+    Q_UNUSED(job);
+    Q_EMIT finished(success);
+    deleteLater();
 }
--- src/ksieveui/managescriptsjob/renamescriptjob.h
+++ src/ksieveui/managescriptsjob/renamescriptjob.h
@@ -1,16 +1,59 @@
+/*
+  Copyright (c) 2016 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
 #ifndef RENAMESCRIPTJOB_H
 #define RENAMESCRIPTJOB_H
 
+#include <QObject>
+#include <QUrl>
+#include "ksieveui_export.h"
+namespace KManageSieve {
+class SieveJob;
+}
 
-class RenameScriptJob : public QObject
+namespace KSieveUi
+{
+class RenameScriptJobPrivate;
+class KSIEVEUI_EXPORT RenameScriptJob : public QObject
 {
     Q_OBJECT
 public:
-    explicit RenameScriptJob(QObject *parent = 0);
+    explicit RenameScriptJob(QObject *parent = Q_NULLPTR);
+    ~RenameScriptJob();
 
-signals:
+    void setOldUrl(const QUrl &url);
+    void setIsActive(bool active);
+    void setNewName(const QString &newName);
 
-public slots:
+    bool canStart() const;
+
+    void start();
+
+Q_SIGNALS:
+    void finished(bool success);
+
+private Q_SLOTS:
+    void slotDeleteResult(KManageSieve::SieveJob *job, bool success);
+    void slotPutScript(KManageSieve::SieveJob *job, bool success);
+    void slotGetResult(KManageSieve::SieveJob *job, bool success, const QString &script, bool isActive);
+
+private:
+    RenameScriptJobPrivate *const d;
 };
-
+}
 #endif // RENAMESCRIPTJOB_H
