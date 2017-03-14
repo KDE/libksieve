66184b693751278c3058db29d9205d2fabe2738b
Montel Laurent <montel@kde.org>
2017-03-14 07:28:18 +0100

    Simplify autotest

--- src/ksieveui/util/autotests/utilmethodtest.cpp
+++ src/ksieveui/util/autotests/utilmethodtest.cpp
@@ -62,11 +62,18 @@ void UtilMethodTest::shouldAssignValue_data()
     data1.sieveCustomPassword = QString();
 
     KSieveUi::Util::AccountInfo info1;
-    info1.sieveUrl = QUrl::fromUserInput(QStringLiteral("sieve://kde:password1@foo.bla.com:543?x-mech=CRAM-MD5"));
+    const QString password = QStringLiteral("password1");
+    const QString userName = QStringLiteral("kde");
+    const int port = 543;
+    info1.sieveUrl = QUrl::fromUserInput(QStringLiteral("sieve://foo.bla.com?x-mech=CRAM-MD5"));
+    info1.sieveUrl.setPassword(password);
+    info1.sieveUrl.setUserName(userName);
+    info1.sieveUrl.setPort(port);
     info1.sieveImapAccountSettings.setServerName(QLatin1String("foo.bla.com"));
-    info1.sieveImapAccountSettings.setPort(543);
-    info1.sieveImapAccountSettings.setUserName(QLatin1String("kde"));
-    info1.sieveImapAccountSettings.setPassword(QLatin1String("password1"));
+    info1.sieveImapAccountSettings.setPort(port);
+    info1.sieveImapAccountSettings.setUserName(userName);
+    info1.sieveImapAccountSettings.setPassword(password);
+
     info1.sieveImapAccountSettings.setAuthenticationType(KSieveUi::SieveImapAccountSettings::Plain);
     //TODO fix encryptionmode
     info1.sieveImapAccountSettings.setEncryptionMode(KSieveUi::SieveImapAccountSettings::AnySslVersion);
