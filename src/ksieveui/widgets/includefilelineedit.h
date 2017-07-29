a19be54eb12c490e327efcc47ab0871d269bad05
Montel Laurent <montel@kde.org>
2017-07-29 16:18:06 +0200

    Start to create a lineedit which lists all local include

---
+++ src/ksieveui/widgets/includefilelineedit.h
@@ -0,0 +1,35 @@
+/*
+   Copyright (C) 2017 Laurent Montel <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef INCLUDEFILELINEEDIT_H
+#define INCLUDEFILELINEEDIT_H
+
+#include <QLineEdit>
+#include "ksieveui_private_export.h"
+
+namespace KSieveUi {
+class KSIEVEUI_TESTS_EXPORT IncludeFileLineEdit : public QLineEdit
+{
+    Q_OBJECT
+public:
+    explicit IncludeFileLineEdit(QWidget *parent = nullptr);
+    ~IncludeFileLineEdit();
+};
+}
+#endif // INCLUDEFILELINEEDIT_H
