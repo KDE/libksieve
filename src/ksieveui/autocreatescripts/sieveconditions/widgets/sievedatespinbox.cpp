KDE/libksieve

commit a71bfae430b84870dd16ad1a452b1fbe2540cef6

GIT_SILENT: Fix 'QStringLiteral(%1).arg(qstring)' into 'qstring'

src/ksieveui/autocreatescripts/sieveconditions/widgets/sievedatespinbox.cpp
@@ -109,7 +109,7 @@ QString SieveDateSpinBox::convertValue() const
         str = QStringLiteral("%1").arg(value(), 2, 10, QLatin1Char('0'));
         break;
     case SelectDateWidget::Weekday:
-        str = QStringLiteral("%1").arg(value());
+        str = value();
         break;
     default:
         qCWarning(LIBKSIEVE_LOG) << "This type mustn't be selected " << mType;

src/ksieveui/autocreatescripts/sieveconditions/sieveconditionconvert.cpp
@@ -81,7 +81,7 @@ QString SieveConditionConvert::code(QWidget *w) const
     const SelectConvertParameterWidget *params = w->findChild<SelectConvertParameterWidget *>(QStringLiteral("params"));
     const QString paramsStr = params->code();
     if (!paramsStr.isEmpty()) {
-        result += QStringLiteral("%1").arg(paramsStr);
+        result += paramsStr;
     }
     return result + AutoCreateScriptUtil::generateConditionComment(comment());
 }

commit c14049267141d43536b390185c05794188ab85ee

SieveDateSpinBox: fix result of code() for weekdays
Found by looking at the broken unit test
sieveeditor-autocreatescripts-sieveeditorgraphicalmodewidgettest

Regressed in a71bfae430b84870dd16ad1a452b1fbe2540cef6

src/ksieveui/autocreatescripts/sieveconditions/widgets/sievedatespinbox.cpp
@@ -96,7 +96,7 @@ QString SieveDateSpinBox::convertValue() const
         str = QStringLiteral("%1").arg(value(), 2, 10, QLatin1Char('0'));
         break;
     case SelectDateWidget::Weekday:
-        str = value();
+        str = QString::number(value());
         break;
     default:
         qCWarning(LIBKSIEVE_LOG) << "This type mustn't be selected " << mType;