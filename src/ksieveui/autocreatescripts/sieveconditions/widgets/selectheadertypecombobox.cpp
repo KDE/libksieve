069b4533ce464915b9e0f376e5309c5646f79dd8
Laurent Montel <montel@kde.org>
2021-11-17 13:43:45 +0100

    GIT_SILENT: use <..> for include

--- src/ksieveui/autocreatescripts/sieveconditions/widgets/selectheadertypecombobox.cpp
+++ src/ksieveui/autocreatescripts/sieveconditions/widgets/selectheadertypecombobox.cpp
@@ -12,16 +12,16 @@
 #include <QPointer>
 #include <QPushButton>
 
-#include "ki18n_version.h"
 #include <KConfigGroup>
 #include <KSharedConfig>
 #include <QDialogButtonBox>
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QVBoxLayout>
+#include <ki18n_version.h>
 
 #if KI18N_VERSION >= QT_VERSION_CHECK(5, 89, 0)
-#include <klazylocalizedstring.h>
+#include <KLazyLocalizedString>
 #undef I18N_NOOP
 #define I18N_NOOP kli18n
 #endif
