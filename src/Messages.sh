0780c9c445da72ff0bdeb40c61399ba6ef7b8bbc
Laurent Montel <montel@kde.org>
2021-12-03 06:59:04 +0100

    GIT_SILENT: enable CI reuse support

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileCopyrightText: none
 $EXTRACTRC `find . -name '*.kcfg'` >> rc.cpp || exit 11
 $XGETTEXT `find . -name \*.cpp | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libksieve.pot
 rm -f rc.cpp
--- src/config-libksieve.h.cmake
+++ src/config-libksieve.h.cmake
@@ -1,3 +1,8 @@
+/*  
+    SPDX-FileCopyrightText: none
+
+    SPDX-License-Identifier: GPL-2.0-only
+*/
 /* The size of a `unsigned long', as computed by sizeof. */
 #define SIZEOF_UNSIGNED_LONG ${SIZEOF_UNSIGNED_LONG}
 
