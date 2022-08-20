549bd98bdc95fb3c23e3c6e7cc1eab85e992af6d
Laurent Montel <montel@kde.org>
2022-08-20 14:16:27 +0200

    Port away from deprecated KIO code

--- src/ksieveui/autocreatescripts/commonwidgets/sievehelpbutton.cpp
+++ src/ksieveui/autocreatescripts/commonwidgets/sievehelpbutton.cpp
@@ -5,7 +5,12 @@
 */
 #include "sievehelpbutton.h"
 
+#include <kio_version.h>
+#if KIO_VERSION >= QT_VERSION_CHECK(5, 98, 0)
+#include <KIO/JobUiDelegateFactory>
+#else
 #include <KIO/JobUiDelegate>
+#endif
 #include <KIO/OpenUrlJob>
 #include <KLocalizedString>
 #include <QIcon>
@@ -27,7 +32,11 @@ bool SieveHelpButton::event(QEvent *event)
     if (event->type() == QEvent::WhatsThisClicked) {
         auto clicked = static_cast<QWhatsThisClickedEvent *>(event);
         auto job = new KIO::OpenUrlJob(QUrl(clicked->href()));
+#if KIO_VERSION >= QT_VERSION_CHECK(5, 98, 0)
+        job->setUiDelegate(KIO::createDefaultJobUiDelegate(KJobUiDelegate::AutoHandlingEnabled, this));
+#else
         job->setUiDelegate(new KIO::JobUiDelegate(KJobUiDelegate::AutoHandlingEnabled, this));
+#endif
         job->start();
         return true;
     }
