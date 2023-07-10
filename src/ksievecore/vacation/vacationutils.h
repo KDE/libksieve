df7dfc5365f910bd153223025954a1b8a7aeab60
Laurent Montel <montel@kde.org>
2023-07-10 13:20:37 +0200

    Move in ksievecore

--- src/ksievecore/vacation/vacationutils.h
+++ src/ksievecore/vacation/vacationutils.h
@@ -60,9 +60,9 @@ Q_REQUIRED_RESULT KSIEVECORE_EXPORT QString composeScript(const Vacation &vacati
 
 Q_REQUIRED_RESULT KSIEVECORE_EXPORT KSieveCore::VacationUtils::Vacation parseScript(const QString &script);
 
-Q_REQUIRED_RESULT QString mergeRequireLine(const QString &script, const QString &scriptUpdate);
+Q_REQUIRED_RESULT KSIEVECORE_EXPORT QString mergeRequireLine(const QString &script, const QString &scriptUpdate);
 
-Q_REQUIRED_RESULT QString updateVacationBlock(const QString &oldScript, const QString &newScript);
+Q_REQUIRED_RESULT KSIEVECORE_EXPORT QString updateVacationBlock(const QString &oldScript, const QString &newScript);
 
 Q_REQUIRED_RESULT KSIEVECORE_EXPORT QString mailAction(MailAction action);
 }
