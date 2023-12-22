2ebe3e25811220f1e0c17e334f9b07467c720da3
Laurent Montel <montel@kde.org>
2023-12-22 13:26:16 +0100

    Fix the translation template version number (add 6 for kf6)
    Make sure that it will not conflict with qt5 version.
    
    @teams/localization

--- src/Messages.sh
+++ src/Messages.sh
@@ -2,5 +2,5 @@
 # SPDX-License-Identifier: CC0-1.0
 # SPDX-FileCopyrightText: none
 $EXTRACTRC `find . -name '*.kcfg'` >> rc.cpp || exit 11
-$XGETTEXT `find . -name \*.cpp | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libksieve.pot
+$XGETTEXT `find . -name \*.cpp | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libksieve6.pot
 rm -f rc.cpp
