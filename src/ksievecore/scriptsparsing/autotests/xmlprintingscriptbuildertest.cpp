6a9a19cfa7190e95b845fc7f6789fa44a1221954
Laurent Montel <montel@kde.org>
2025-03-04 07:57:29 +0100

    Fix autotest on windows

--- src/ksievecore/scriptsparsing/autotests/xmlprintingscriptbuildertest.cpp
+++ src/ksievecore/scriptsparsing/autotests/xmlprintingscriptbuildertest.cpp
@@ -115,6 +115,33 @@ void XMLPrintingScriptBuilderTest::shouldCreateXml_data()
     QTest::newRow("test-date1") << QStringLiteral("test-date1") << true;
 }
 
+void diffFile(const QString &refFile, const QString &generatedFile)
+{
+    QProcess proc;
+#ifdef _WIN32
+    QStringList args = QStringList() << QStringLiteral("Compare-Object") << QString(QStringLiteral("(Get-Content %1)")).arg(refFile)
+                                     << QString(QStringLiteral("(Get-Content %1)")).arg(generatedFile);
+
+    proc.start(QStringLiteral("powershell"), args);
+    QVERIFY(proc.waitForFinished());
+
+    auto pStdOut = proc.readAllStandardOutput();
+    if (pStdOut.size()) {
+        qDebug() << "Files are different, diff output message:\n" << pStdOut.toStdString().c_str();
+    }
+
+    QCOMPARE(pStdOut.size(), 0);
+#else
+    // compare to reference file
+    const QStringList args = QStringList() << QStringLiteral("-u") << refFile << generatedFile;
+
+    proc.setProcessChannelMode(QProcess::ForwardedChannels);
+    proc.start(QStringLiteral("diff"), args);
+    QVERIFY(proc.waitForFinished());
+    QCOMPARE(proc.exitCode(), 0);
+#endif
+}
+
 void XMLPrintingScriptBuilderTest::shouldCreateXml()
 {
     QFETCH(QString, input);
@@ -136,13 +163,7 @@ void XMLPrintingScriptBuilderTest::shouldCreateXml()
     f.write(resultStr.toUtf8());
     f.close();
 
-    // compare to reference file
-    QStringList args = QStringList() << QStringLiteral("-u") << refFile << generatedFile;
-    QProcess proc;
-    proc.setProcessChannelMode(QProcess::ForwardedChannels);
-    proc.start(QStringLiteral("diff"), args);
-    QVERIFY(proc.waitForFinished());
-    QCOMPARE(proc.exitCode(), 0);
+    diffFile(refFile, generatedFile);
 }
 
 QTEST_MAIN(XMLPrintingScriptBuilderTest)
