6219b52cfb741ca99e5e8eb2c1cf13a942fde73e
Laurent Montel <montel@kde.org>
2026-08-31 13:19:50 +0200

    Use static QStringList

--- src/ksieveui/editor/sievetextedit.cpp
+++ src/ksieveui/editor/sievetextedit.cpp
@@ -182,18 +182,13 @@ void SieveTextEdit::slotUpdateLineNumberArea(const QRect &rect, int dy)
 
 QStringList SieveTextEdit::completerList() const
 {
-    QStringList listWord;
-
-    listWord << u"require"_s << u"stop"_s;
-    listWord << u":contains"_s << u":matches"_s << QStringLiteral(":is") << QStringLiteral(":over") << QStringLiteral(":under") << u":all"_s << u":domain"_s
-             << QStringLiteral(":localpart");
-    listWord << u"if"_s << u"elsif"_s << QStringLiteral("else");
-    listWord << u"keep"_s << u"reject"_s << QStringLiteral("discard") << QStringLiteral("redirect") << QStringLiteral("addflag") << u"setflag"_s;
-    listWord << u"address"_s << u"allof"_s << QStringLiteral("anyof") << QStringLiteral("exists") << QStringLiteral("false") << u"header"_s << u"not"_s
-             << QStringLiteral("size") << QStringLiteral("true");
-    listWord << u":days"_s << u":seconds"_s << QStringLiteral(":subject") << QStringLiteral(":addresses") << QStringLiteral(":text");
-    listWord << u":name"_s << u":headers"_s << QStringLiteral(":first") << QStringLiteral(":importance");
-    listWord << u":message"_s << u":from"_s;
+    static const QStringList listWord{
+        u"require"_s,  u"stop"_s,       u":contains"_s, u":matches"_s, u":is"_s,         u":over"_s,    u":under"_s,   u":all"_s,
+        u":domain"_s,  u":localpart"_s, u"if"_s,        u"elsif"_s,    u"else"_s,        u"keep"_s,     u"reject"_s,   u"discard"_s,
+        u"redirect"_s, u"addflag"_s,    u"setflag"_s,   u"address"_s,  u"allof"_s,       u"anyof"_s,    u"exists"_s,   u"false"_s,
+        u"header"_s,   u"not"_s,        u"size"_s,      u"true"_s,     u":days"_s,       u":seconds"_s, u":subject"_s, u":addresses"_s,
+        u":text"_s,    u":name"_s,      u":headers"_s,  u":first"_s,   u":importance"_s, u":message"_s, u":from"_s,
+    };
 
     return listWord;
 }
